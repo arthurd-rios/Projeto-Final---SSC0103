87ec5cef35545aa5b70b2e3c1b0f70230b81b190
arthurd-rios <arthurd.rios10@gmail.com>
2025-06-11 15:56:22 -0300

    Organização

---
+++ Curso.py
@@ -0,0 +1,60 @@
+class Curso:
+
+    def __init__(self, nome, unidade, duracaoideal, duracaomin, duracaomax, obrigatorias, optlivres, opteletivas):
+
+        self.nome = nome
+        self.unidade = unidade
+        self.duracaoideal = duracaoideal
+        self.duracaomin = duracaomin
+        self.duracaomax = duracaomax
+        self.obrigatorias = obrigatorias
+        self.optlivres = optlivres
+        self.opteletivas = opteletivas
+
+    def getNome(self):
+        return self.nome
+    
+    def setNome(self, valor):
+        self.nome = valor  
+
+    def getUnidade(self):
+        return self.unidade
+    
+    def setUnidade(self, valor):
+        self.unidade = valor  
+
+    def getDuracaoIdeal(self):
+        return self.duracaoideal
+    
+    def setDuracaoIdeal(self, valor):
+        self.duracaoideal = valor  
+
+    def getDuracaoMinima(self):
+        return self.duracaomin
+    
+    def setDuracaoMinima(self, valor):
+        self.duracaomin = valor  
+
+    def getDuracaoMaxima(self):
+        return self.duracaomax
+    
+    def setDuracaoMaxima(self, valor):
+        self.duracaomax = valor  
+
+    def getCodigo(self):
+        return self.codigo
+    
+    def setCodigo(self, valor):
+        self.codigo = valor  
+
+    def getCodigo(self):
+        return self.codigo
+    
+    def setCodigo(self, valor):
+        self.codigo = valor  
+
+    def getCodigo(self):
+        return self.codigo
+    
+    def setCodigo(self, valor):
+        self.codigo = valor  
---
+++ Unidade.py
@@ -0,0 +1,10 @@
+class Unidade:
+
+    def __init__(self, unidade):
+        self.unidade = unidade
+
+    def getUnidade(self):
+        return self.unidade
+    
+    def setUnidade(self, valor):
+        self.unidade = valor
